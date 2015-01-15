cc9b884236a16bd5ebe80b184503ce221f7190ae
Jason Lu <jasonl.biz@gmail.com>
2015-01-14 23:23:34 -0800

    minor update

--- models/news.rb
+++ models/news.rb
@@ -1,6 +1,7 @@
 class News < ActiveRecord::Base
   attr_accessible :user_id, :close_at, :content, :publish_at, :title
   belongs_to :admin, :foreign_key => 'user_id'
+  belongs_to :user, :foreign_key => 'user_id'
 
   scope :letest, -> { where("publish_at IS NOT NULL AND (close_at IS NULL OR close_at >= ?)", DateTime.now).order(publish_at: :desc) }
 end
