637f2b7a6e3bb979dbbf0829aafe0dd43e5b8b15
Jason Lu <jasonl.biz@gmail.com>
2015-01-26 00:38:11 -0800

    update deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@onlynet.biz}
+set :deploy_to, "/srv/www/201_onlynet_prod_models"
+set :static_shares, "/srv/www/202_onlynet_prod_static"
 set :env, "production"
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ set :env, "production"
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+
 
 
 # Custom SSH Options
